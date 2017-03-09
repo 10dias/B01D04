a9407af8a1ef292084a3e9e5c29ea209fb84b7f2
thassiov <tvmcarvalho@gmail.com>
2017-03-09 10:42:50 -0300

    Adicionando chamada a API

--- beer.js
+++ beer.js
@@ -1,5 +1,24 @@
 /**
 * @author Thassio Victor <tvmcarvalho@gmail.com>
-* @desc Listar cervejas da API https://api.punkapi.com
+* @desc Listar cervejas da API httpss://api.punkapi.com
     *
     */
+
+'use strict';
+
+const https = require('https');
+
+const API = 'https://api.punkapi.com/v2/beers';
+
+https.get(API, function(res) {
+    var finalRes = '';
+
+    res.on('data', function(d) {
+        finalRes +=d;
+    });
+
+    res.on('end', function() {
+        console.log(finalRes);
+    });
+});
+
